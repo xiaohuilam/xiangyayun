011c187ca8b69c34485a8b3f2353ec9718c0de9f
MeanShare <chanmir@qq.com>
2021-11-17 21:01:39 +0800

    123

--- App/Service/WechatService.php
+++ App/Service/WechatService.php
@@ -124,48 +124,6 @@ class WechatService
     {
         $officialAccount = Factory::officialAccount(config('WECHAT'));
         $server = $officialAccount->server;
-        $server->push(function (\EasySwoole\WeChat\Kernel\Contracts\MessageInterface $message) {
-            var_dump($message);
-            $data = $message->transformForJsonRequest();
-            var_dump($data);
-            switch ($message->getType()) {
-                case 'event':
-                    if ($data['EventKey'] == "QRCODE_LOGIN") {
-                        return self::QRCODE_LOGIN($data);
-                    }
-                    if ($data['EventKey'] == "QRCODE_LOGIN") {
-                        return self::QRCODE_BIND($data);
-                    }
-                    break;
-                case 'text':
-                    $text = '收到文字消息';
-                    break;
-                case 'image':
-                    $text = '收到图片消息';
-                    break;
-                case 'voice':
-                    $text = '收到语音消息';
-                    break;
-                case 'video':
-                    $text = '收到视频消息';
-                    break;
-                case 'location':
-                    $text = '收到坐标消息';
-                    break;
-                case 'link':
-                    $text = '收到链接消息';
-                    break;
-                case 'file':
-                    $text = '收到文件消息';
-                    break;
-                // ... 其它消息
-                default:
-                    $text = '收到其它消息';
-                    break;
-            }
-
-            return new \EasySwoole\WeChat\Kernel\Messages\Text($text);
-        });
         return $server;
     }
 
